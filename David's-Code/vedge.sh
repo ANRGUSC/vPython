b8b6bb7c127aa07bc6f2a49ba4982a7f1002c799
gowrivs <gshan.r@gmail.com>
2020-10-02 12:41:42 -0700

    readme updated

--- David's-Code/vedge.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-DIRECTORY='cover_dir-'$(date +'%b-%d-%Y->%H:%M:%S%Z')
-echo $DIRECTORY
-mkdir $DIRECTORY
-touch $DIRECTORY/trace.log
-python3.5 -m trace -g -s --count $1 >> $DIRECTORY/trace.log
-#python3.5 -m trace -g -s --count -C $DIRECTORY/ -tg -m cProfile $1 >> $DIRECTORY/trace.log
-
